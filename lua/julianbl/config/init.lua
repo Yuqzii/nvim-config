11b9c4ba3a5726fe31f5545ca3703175259d5736
Yuqzii <p4r4yuqzii@gmail.com>
2025-05-16 23:20:17 +0200

    autoformat on save for golang

--- lua/julianbl/config/init.lua
+++ lua/julianbl/config/init.lua
@@ -35,3 +35,16 @@ vim.cmd("autocmd WinLeave * set colorcolumn=0")
 vim.cmd("autocmd WinEnter * set colorcolumn=" .. colcolumn)
 
 vim.o.mouse = ""
+
+vim.api.nvim_create_augroup("AutoFormat", {})
+vim.api.nvim_create_autocmd(
+	"BufWritePost",
+	{
+		pattern = "*.go",
+		group = "AutoFormat",
+		callback = function()
+			vim.cmd("silent !gofmt -w -s ./")
+			vim.cmd("edit")
+		end,
+	}
+)
