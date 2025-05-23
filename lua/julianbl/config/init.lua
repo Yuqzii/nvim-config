75e43c86c2d08fa958ababab189f9012d3c48584
yuqzii <p4r4yuqzii@gmail.com>
2025-05-23 17:38:30 +0200

    translucent background

--- lua/julianbl/config/init.lua
+++ lua/julianbl/config/init.lua
@@ -51,3 +51,10 @@ vim.api.nvim_create_autocmd(
 		end,
 	}
 )
+
+vim.cmd [[
+	highlight Normal guibg=none
+	highlight NonText guibg=none
+	highlight Normal ctermbg=none
+	highlight NonText ctermbg=none
+]]
