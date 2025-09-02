1aacfd8d0b49e1223ac0d1688467dd59d9d1612c
Yuqzii <p4r4yuqzii@gmail.com>
2025-09-02 18:58:58 +0200

    change go icon

---
+++ lua/julianbl/plugins/nvim-web-devicons.lua
@@ -0,0 +1,31 @@
+return {
+	"nvim-tree/nvim-web-devicons",
+	config = function()
+		require("nvim-web-devicons").setup({
+			default = true,
+			strict = true,
+			variant = "dark",
+
+			override_by_extension = {
+				["go"] = {
+					icon = "󰟓",
+					color = "#00aed8",
+					name = "Go",
+				},
+			},
+
+			override_by_filename = {
+				["go.mod"] = {
+					icon = "󰟓",
+					color = "#00aed8",
+					name = "Go",
+				},
+				["go.sum"] = {
+					icon = "󰟓",
+					color = "#00aed8",
+					name = "Go",
+				},
+			},
+		})
+	end
+}
